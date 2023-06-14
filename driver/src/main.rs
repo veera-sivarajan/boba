d4ec3348ab781870f1cc7f00ac60f60df7593cd8
Veera <sveera.2001@gmail.com>
2023-06-14 17:24:58 -0400

    Cleanup

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -2,24 +2,28 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::process::Command;
 
+fn assemble(header: String, code: String, runtime: String) -> std::io::Result<()> {
+    let mut assembly_file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open("/home/veera/projects/boba/output/math.s")?;
+    assembly_file.write_all(header.as_bytes())?;
+    assembly_file.write_all(code.as_bytes())?;
+    assembly_file.write_all(runtime.as_bytes())?;
+    Command::new("gcc")
+        .arg("/home/veera/projects/boba/output/math.s")
+        .arg("-o")
+        .arg("/home/veera/projects/boba/output/math")
+        .output()
+        .expect("Failed to invoke gcc.");
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     let source = include_str!("/home/veera/projects/boba/test/math.rs");
     match compiler::compile(source) {
         Ok(assembly) => {
-            let mut assembly_file = OpenOptions::new()
-                .create(true)
-                .write(true)
-                .open("/home/veera/projects/boba/output/math.s")?;
-            assembly_file.write_all(assembly.header.as_bytes())?;
-            assembly_file.write_all(assembly.code.as_bytes())?;
-            assembly_file.write_all(assembly.runtime.as_bytes())?;
-            Command::new("gcc")
-                .arg("/home/veera/projects/boba/output/math.s")
-                .arg("-o")
-                .arg("/home/veera/projects/boba/output/math")
-                .output()
-                .expect("Failed to invoke gcc.");
-            Ok(())
+            assemble(assembly.header, assembly.code, assembly.runtime)
         }
         Err(err) => {
             eprintln!("{err}");
