405c0302e01b92cf83fbfcabd6740a6a8ef8bd10
Veera <sveera.2001@gmail.com>
2023-06-14 14:01:38 -0400

    Make frontend create a executable using gcc

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,12 +1,30 @@
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::process::Command;
 
-fn main() {
-    let source = include_str!("/home/veera/projects/boba/test/test.rs");
+fn main() -> std::io::Result<()> {
+    let source = include_str!("/home/veera/projects/boba/test/math.rs");
     match compiler::compile(source) {
         Ok(assembly) => {
-            println!("{}", assembly.header);
-            println!("{}", assembly.code);
-            println!("{}", assembly.runtime);
+            let mut assembly_file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .append(true)
+                .open("math.s")?;
+            assembly_file.write_all(assembly.header.as_bytes())?;
+            assembly_file.write_all(assembly.code.as_bytes())?;
+            assembly_file.write_all(assembly.runtime.as_bytes())?;
+            Command::new("gcc")
+                .arg("math.s")
+                .arg("-o")
+                .arg("math")
+                .output()
+                .expect("Failed to invoke gcc.");
+            Ok(())
         }
-        Err(err) => eprintln!("{err}")
+        Err(err) => {
+            eprintln!("{err}");
+            Ok(())
+        } 
     }
 }
