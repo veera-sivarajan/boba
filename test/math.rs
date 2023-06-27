c7fbb76759d57bf0fbec47a5f88ace6ac11d10ac
Veera <sveera.2001@gmail.com>
2023-06-27 02:09:29 -0400

    Println is broke

--- test/math.rs
+++ test/math.rs
@@ -1,20 +1,6 @@
-let global = 100;
-
-fn dummy() -> Number {}
-
-fn factorial(x: Number, y: Number) -> Number {
-    let apple = 10;
-    let ball = 20;
-    if x > y {
-        let car = 30;
-        println(apple + car);
-    } else {
-        let apple = 100;
-        let ball = dummy();
-        println(apple + ball);
-    }
-}
-
+let what = 10;
 fn main() -> Number {
-    factorial();
+    let local = 20;
+    let last = local + what;
+    println(last);
 }
