dd08d2e755e381e9c9bd962cdd6efc157e54d461
Veera <sveera.2001@gmail.com>
2023-08-02 14:41:44 -0500

    Update test suite

--- test/greet-with-name.rs
+++ test/greet-with-name.rs
@@ -1,9 +1,19 @@
 fn greet(name: str) {
-    println("Hello, {}", name);
+    println("Hello, {}! Hope you're doing great!!", name);
+}
+
+
+fn greet_in_spanish(name: str) {
+    println("Hola, {}", name);
 }
 
 fn main() {
     greet("billa");
     greet("liliana");
     greet("toby");
+
+    
+    greet_in_spanish("billa");
+    greet_in_spanish("liliana");
+    greet_in_spanish("toby");
 }
